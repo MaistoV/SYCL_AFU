a9b0cedefe6f7c801c4bd33ce4d9fea3a3ee847c
Vincenzo Maisto <maisto_v@libero.it>
2024-06-06 14:49:55 +0200

    [Measures][Power] Update measuring tool to sysfs reads
    * This directly accessesthr DFL driver and removes the needfor sharing the OPAE library with the running application
    
    * Still no appriciatable difference during measures when GBS application is running, e.g. make test_gbs

--- measures/power/measure_power.sh
+++ measures/power/measure_power.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-# Simple power measurement of Hitek xC220 cards using OPAE-SDK
-output_file=power.csv
-if [[ "$1" != "" ]]; then
-    output_file=$1
-fi
+# Sysfs path of target hwmon
+SYSFS_PATH=$1
 
-# Smaple and dump to tmp file
-tmp_file=fpgainfo_power.tmp
-fpgainfo bmc > $tmp_file
+# Simple power measurement of Hitek xC220 cards using OPAE-SDK
+OUTPUT_FILE=power.csv
+if [[ "$2" != "" ]]; then
+    OUTPUT_FILE=$2
+fi
 
 # Get timestamp
 timestamp=$(date +"%s.%N")
 
 # Grep from file
-# Assuming only PCIe power supply, grep 12V and 3.3V power rails
-volt_12V=$(grep "PCIe 12V Power Monitor 2 Voltage" $tmp_file | awk '{print $10}' | sed "s/,/./g")
-amp_12V=$(grep "PCIe 12V Power Monitor 1 Current" $tmp_file  | awk '{print $10}' | sed "s/,/./g")
-volt_3V3=$(grep "PCIe 3V3 Power Monitor 2 Voltage" $tmp_file | awk '{print $9}'  | sed "s/,/./g")
-amp_3V2=$(grep "PCIe 3V3 Power Monitor 1 Current" $tmp_file  | awk '{print $9}'  | sed "s/,/./g")
+# Assuming only PCIe power supply; cat data for 12V and 3.3V power rails
+volt_12V=$(cat $SYSFS_PATH/in4_input)       # in4_label = PCIe 12V Power Monitor 2 Voltage
+amp_12V=$(cat $SYSFS_PATH/curr3_input)      # curr3_label = PCIe 12V Power Monitor 1 Current
+volt_3V3=$(cat $SYSFS_PATH/in3_input)       # in3_label = PCIe 3V3 Power Monitor 2 Voltage
+amp_3V3=$(cat $SYSFS_PATH/curr2_input)      # curr2_label = PCIe 3V3 Power Monitor 1 Current
+board_power=$(cat $SYSFS_PATH/power1_input) # power1_label = Board Power
 
 # Append raw measures to file
-# Expecting header "12V Voltage, 12V Current, 3V3 Voltage, 3V3 Current"
-echo "$timestamp,$volt_12V,$amp_12V,$volt_3V3,$amp_3V2" >> $output_file
-
-# Remove tmp file
-rm $tmp_file
+# Expecting header "12V Voltage; 12V Current; 3V3 Voltage; 3V3 Current, Board Power"
+echo "$timestamp;$volt_12V;$amp_12V;$volt_3V3;$amp_3V3;$board_power" >> $OUTPUT_FILE
--- measures/power/measure_power_top.sh
+++ measures/power/measure_power_top.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-output_file=$1/power.csv
-
-# Create if does not exist
-if [ ! -e $output_file ]; then
-    echo "Time(s),12V Voltage(V), 12V Current(A), 3V3 Voltage(V), 3V3 Current(A)" > $output_file
+# Output file
+OUTPUT_FILE=power.csv
+if [[ "$1" != "" ]]; then
+    OUTPUT_FILE=$1/power.csv
 fi
 
-# Launch a busy loop, polling with fpgainfo
-# NOTE: this has a round-trip-time of aboud 0.5 seconds
+# Sleep time between sysfs reads
+# If much larger than the file access time, this could approximate the sampling period
+SLEEP_TIME=0.5
+
+# This changes across boots
+BASE_SYSFS_PATH=/sys/bus/pci/devices/${PAC_PCIE_SBD}.0/fpga_region/region0/dfl-fme.0
+SYSFS_PATH=$(find ${BASE_SYSFS_PATH} -name n6000bmc-hwmon.2.auto)/hwmon/hwmon3
+
+# Create if does not exist
+if [ ! -e $OUTPUT_FILE ]; then
+    echo "Time(s);12V Voltage(mV);12V Current(mA);3V3 Voltage(mV);3V3 Current(mA);Board Power(uW)" > $OUTPUT_FILE
+fi
+
+# Launch a busy loop, polling with subscript
 cnt=0
 while (true) do
     cnt=$((cnt+1))
     echo "Collecting sample #$cnt, kill to stop"
     # Launch measure
-    ${ROOT_DIR}/measures/power/measure_power.sh $output_file
+    ${ROOT_DIR}/measures/power/measure_power.sh \
+        $SYSFS_PATH \
+        $OUTPUT_FILE
+    # Sleep between samples
+    sleep $SLEEP_TIME
 done
