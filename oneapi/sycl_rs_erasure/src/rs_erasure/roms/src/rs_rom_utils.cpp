fd1edfa35101b3d9236f88abf071d232d1ca2b2f
Vincenzo Maisto <vincenzo.maisto2@unina.it>
2024-06-28 06:30:21 -0400

    [VFP] Adjust debug output format in Java syntax for RSPreCoded class

--- oneapi/sycl_rs_erasure/src/rs_erasure/roms/src/rs_rom_utils.cpp
+++ oneapi/sycl_rs_erasure/src/rs_erasure/roms/src/rs_rom_utils.cpp
@@ -214,7 +214,7 @@ void print_matrix_2d(FILE* fd, int num_rows, int num_cols, unsigned char *s, con
 void print_contiguous_cell ( FILE* fd, uint8_t* array, unsigned int num_cells, unsigned int cell_length, unsigned int line_byte_width ) {
 	for ( unsigned int i = 0; i < num_cells; i++ ) {
 		for ( unsigned int l = 0; l < cell_length; l++ ) {
-			fprintf(fd, "%02x ", ((uint8_t(*)[cell_length])array)[i][l]);
+			fprintf(fd, "(byte)0x%02x, ", ((uint8_t(*)[cell_length])array)[i][l]);
 			if ( ((l+1) % line_byte_width) == 0 ) {
 				fprintf(fd, "\n");
 			}
