953eeded089dabccca9fa5e8191e144367cec50e
Vincenzo Maisto <vincenzo.maisto2@unina.it>
2024-06-28 06:38:25 -0400

    [Install] Add Gigapages to installation

--- install/rh8_9/rh8_9_install.sh
+++ install/rh8_9/rh8_9_install.sh
@@ -14,7 +14,8 @@ cd $PACKAGE_DIR/rpm/linux-dfl
 
 sudo dnf localinstall -y kernel-*.rpm
 
-sudo sed -i 's/GRUB_CMDLINE_LINUX="[^"]*/& intel_iommu=on pcie=realloc default_hugepagesz=2MB hugepagesz=1G hugepages=2 hugepagesz=2M hugepages=200/' /etc/default/grub
+# NOTE: We need a Gigapage for each VFProxy
+sudo sed -i 's/GRUB_CMDLINE_LINUX="[^"]*/& intel_iommu=on pcie=realloc default_hugepagesz=2MB hugepagesz=1G hugepages=6 hugepagesz=2M hugepages=200/' /etc/default/grub
 sudo grub2-mkconfig > /dev/null
 
 echo "Reboot system and check:"
