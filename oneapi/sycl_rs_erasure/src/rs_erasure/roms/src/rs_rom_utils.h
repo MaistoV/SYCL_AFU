1b2c8d4f9b6945dc8e00023f2a5e2658dbef71ed
Vincenzo Maisto <vincenzo.maisto2@unina.it>
2025-06-26 19:50:04 +0200

    [oneapi] Minor fix to ROM utils

--- oneapi/sycl_rs_erasure/src/rs_erasure/roms/src/rs_rom_utils.h
+++ oneapi/sycl_rs_erasure/src/rs_erasure/roms/src/rs_rom_utils.h
@@ -1,9 +1,9 @@
 #ifndef _RS_ROM_UTILS_H
 #define _RS_ROM_UTILS_H
 
-#include <isa-l.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <getopt.h>
 #include <isa-l.h>	// linking against installed
@@ -20,8 +20,8 @@ int gf_gen_decode_matrix_simple(uint8_t * encode_matrix,
 						uint8_t * decode_matrix,
 						uint8_t * invert_matrix,
 						uint8_t * temp_matrix,
-						uint8_t * decode_index, uint8_t * erasure_list, 
-						int nerrs, int k, int m); 
+						uint8_t * decode_index, uint8_t * erasure_list,
+						int nerrs, int k, int m);
 int gf_gen_decode_matrix(
 				uint8_t 	*encode_matrix,
 				uint8_t 	*decode_matrix,
@@ -45,15 +45,15 @@ int gen_1_erasure_patterns ( int k, int p, uint8_t* erasure_patterns );
 int gen_erasure_patterns   ( const int k, const int p, uint8_t* erasure_patterns );
 int gen_survival_patterns  ( const int k, const int p, uint8_t* survival_patterns );
 void erasures_to_bitstring ( int k, int p, int num_errors, uint8_t* erasure_pattern, uint8_t* return_buffer );
-void convert_binary_permutations_to_array ( const int rs_k, const int rs_p, 
-											const int survival_vectors_per_erasure, 
+void convert_binary_permutations_to_array ( const int rs_k, const int rs_p,
+											const int survival_vectors_per_erasure,
 											const uint8_t* permutations, uint8_t* survival_pattern );
-void convert_bitstring_to_array ( 
-									const int len_single_string, 
-									const int num_substrings, 
-									const int high_bits, 
+void convert_bitstring_to_array (
+									const int len_single_string,
+									const int num_substrings,
+									const int high_bits,
 									const uint8_t* bitstring,
-									uint8_t* int_array 
+									uint8_t* int_array
 									);
 
 #define _1d_print
@@ -62,5 +62,5 @@ void convert_bitstring_to_array (
 #else
 #define print_matrix(fd, num_rows, num_cols, s, msg) print_matrix_2d(fd, num_rows, num_cols, s, msg)
 #endif
- 
+
 #endif // _RS_ROM_UTILS_H
